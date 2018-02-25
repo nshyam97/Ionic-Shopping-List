def798e9e3556691a4e0bbb0154fdc3a4870a225
nshyam97 <nshyam668@gmail.com>
2018-02-25 00:25:32 +0000

    Initial App (#1)

---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyC_qQoLc4VIWktspgP1FfbNxvegVgQze3U",
+    authDomain: "ionicfirebase-e8d5e.firebaseapp.com",
+    databaseURL: "https://ionicfirebase-e8d5e.firebaseio.com",
+    projectId: "ionicfirebase-e8d5e",
+    storageBucket: "",
+    messagingSenderId: "51208756940"
+};
